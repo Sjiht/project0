361cd7972af0d105118e1f4f869ef8147e87afcb
Patrick de Koning <patrickdekoning@live.nl>
2012-02-29 15:21:33 +0100

    Get XML

---
+++ patrick/xml/hodex_nl_uva_1013.php
@@ -0,0 +1,41 @@
+<?php
+$xmlstr = <<<XML
+<program xmlns="http://www.hodex.nl" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.hodex.nl http://www.hodex.nl/hodex.xsd"><editor><![CDATA[editor@hodexer.nl]]></editor><expires>2012-02-13</expires><generator><![CDATA[hodexer]]></generator><hodexVersion><![CDATA[2.5.8]]></hodexVersion><lastEdited>2012-02-03</lastEdited><programClassification><accreditation><accreditationDate>2014-12-31</accreditationDate><orgUnitCountry>nl</orgUnitCountry><orgUnitName><![CDATA[NVAO]]></orgUnitName><webLink xml:lang='nl'>http://www.nvao.net</webLink></accreditation><crohoCode>60202</crohoCode><degree>MSc</degree><fieldOfEducation classification='SOI2006'>5141</fieldOfEducation><financing>government</financing><numerusFixus xsi:nil='true'></numerusFixus><orgUnitId>uva</orgUnitId><orgUnitLocation><![CDATA[Amsterdam]]></orgUnitLocation><programCredits>120</programCredits><programDuration unit="month">24</programDuration><programForm>full-time</programForm><programId>1013</programId><programLevel>academic master</programLevel><programType>regular</programType><startingYear>2012</startingYear><studyCluster>technology</studyCluster><studyCluster>nature</studyCluster></programClassification><programContacts></programContacts><programCurriculum><instructionLanguage><languageCode>en</languageCode><percentage>100</percentage></instructionLanguage><percentageDistanceLearning>0</percentageDistanceLearning><percentageNightEducation>0</percentageNightEducation><sizeOfTutorialGroups xsi:nil='true'></sizeOfTutorialGroups><teachingTypesInCurriculumYear year='1'><teachingTypeInCurriculum><typeOfTeaching>theory</typeOfTeaching>
+<percentage>0</percentage></teachingTypeInCurriculum><teachingTypeInCurriculum><typeOfTeaching>practice</typeOfTeaching>
+<percentage>0</percentage></teachingTypeInCurriculum><teachingTypeInCurriculum><typeOfTeaching>self study</typeOfTeaching>
+<percentage>0</percentage></teachingTypeInCurriculum></teachingTypesInCurriculumYear></programCurriculum><programDescriptions><contentLink><contentSummary xml:lang="nl"><![CDATA[Graduates can apply for PhD positions at the Nikhef and other particle and astroparticle physics institutes around the world. A Nikhef PhD includes one to two years spent abroad at an institute like the CERN. 
+
+Research options within the Netherlands include positions in the laboratories of large international companies.
+
+The students' training in and familiarity with particle detection and/or cutting edge computing techniques can lead to careers in the medical laboratory or software sector.]]></contentSummary><contentType>website</contentType><subject>job perspective</subject><webLink xml:lang="nl"><![CDATA[http://studieadres.nl]]></webLink></contentLink><programDescription xml:lang="nl"><![CDATA[The programme is looking for students who are interested in:
+
+■the smallest particles in the Universe, 
+■the way these particles interact, 
+■matter and anti-matter, 
+■the Higgs particle, 
+■neutrinos from supernovae or from our Sun, 
+■the search for dark matter and, 
+■the detection of gravitational waves.
+
+This Master's track offers the chance to reflect on current research topics such as:
+
+■the search for the Higgs particles;
+■physics beyond the Standard Model;
+■the first detection of cosmic neutrinos; gravitational waves or dark matter particles.
+]]></programDescription><programDescription xml:lang="en"><![CDATA[The programme is looking for students who are interested in:
+
+■the smallest particles in the Universe, 
+■the way these particles interact, 
+■matter and anti-matter, 
+■the Higgs particle, 
+■neutrinos from supernovae or from our Sun, 
+■the search for dark matter and, 
+■the detection of gravitational waves.
+
+This Master's track offers the chance to reflect on current research topics such as:
+
+■the search for the Higgs particles;
+■physics beyond the Standard Model;
+■the first detection of cosmic neutrinos; gravitational waves or dark matter particles.]]></programDescription><programName xml:lang="nl"><![CDATA[Physics: Particle and Astroparticle Physics]]></programName><programName xml:lang="en"><![CDATA[Physics: Particle and Astroparticle Physics]]></programName><programSummary xml:lang="en"><![CDATA[Particle and Astroparticle Physics is a track of the Master's programme in Physics.]]></programSummary><programSummary xml:lang="nl"><![CDATA[Particle and Astroparticle Physics is a track of the Master's programme in Physics.]]></programSummary><searchword xml:lang="nl"></searchword><webLink xml:lang="nl">http://www.studeren.uva.nl/ma-particle-astroparticle-physics</webLink><webLink xml:lang="en">http://studeren.uva.nl/msc_particle_and_astroparticle_physics</webLink></programDescriptions><programInternationalization><ErasmusMundusProgram>0</ErasmusMundusProgram><facilitationForeignScholarships xsi:nil='true'></facilitationForeignScholarships><foreignStay xsi:nil='true'></foreignStay><maxForeignCredits xsi:nil='true'></maxForeignCredits><percentageForeignStudents xsi:nil='true'></percentageForeignStudents><percentageForeignTeachers xsi:nil='true'></percentageForeignTeachers><percentageStudentsAbroad xsi:nil='true'></percentageStudentsAbroad><percentageTeachersAbroad xsi:nil='true'></percentageTeachersAbroad><studyAbroad xsi:nil='true'></studyAbroad></programInternationalization><programRelations></programRelations><programOrganization><faculty xml:lang="en">Faculty of Science</faculty><faculty xml:lang="nl">faculteit der Natuurwetenschappen, Wiskunde en Informatica</faculty><programRun><applicationDeadline xsi:nil="true"/><applicationDeadlineNonEU xsi:nil="true"/><endDate>2014-08-31</endDate><startingDate>2012-09-01</startingDate></programRun><programRun><applicationDeadline xsi:nil="true"/><applicationDeadlineNonEU xsi:nil="true"/><endDate>2015-08-31</endDate><startingDate>2013-09-01</startingDate></programRun><programRun><applicationDeadline xsi:nil="true"/><applicationDeadlineNonEU xsi:nil="true"/><endDate>2016-08-31</endDate><startingDate>2014-09-01</startingDate></programRun><programStart><applicationDeadline xsi:nil='true'></applicationDeadline><applicationDeadlineNonEU xsi:nil='true'></applicationDeadlineNonEU><startingMonth>September</startingMonth></programStart></programOrganization><programFree><crohoName></crohoName><dl>nl</dl><studyClusterUvA>Aarde Natuur en Milieu</studyClusterUvA><studyClusterUvA>Beta</studyClusterUvA><studyClusterUvA>Techniek Ontwerp en Innovatie</studyClusterUvA><pp></pp><ps></ps><cq></cq><cp></cp><orgUnitLocationId><![CDATA[Amsterdam]]></orgUnitLocationId><facultyId><![CDATA[nwi]]></facultyId><targetMarket>nl</targetMarket><targetMarket>int</targetMarket><programPhase>initial</programPhase></programFree></program>
+XML;
+?>
