656589cba330fbfb79d25f96eaf551d4255b9578
Patrick de Koning <patrickdekoning@live.nl>
2012-02-29 17:52:49 +0100

    XML 3

--- patrick/getxml.php
+++ patrick/getxml.php
@@ -3,22 +3,23 @@
 $n = 1;
 while( $n < 1100 )
 {
-	$serverfileurl = "http://www.hodexer.nl/hodex/uva/hodex_nl_uva_" . $n . "_dual.xml";
+	$serverfileurl = "http://www.hodexer.nl/hodex/uva/hodex_nl_uva_" . $n . ".xml";
 	$serverfile = file_get_contents($serverfileurl);
 	
-	$localfileurl = "dual/hodex_nl_uva_" . $n . "_dual.php";
+	if ($serverfile != '')
+	{
+		$localfileurl = "xml/hodex_nl_uva_" . $n . ".php";
 	
-	$text = str_replace('<?xml version="1.0" encoding="utf-8"?>', '<?php
+		$text = str_replace('<?xml version="1.0" encoding="utf-8"?>', '<?php
 $xmlstr = <<<XML
 ', $serverfile);
 	
-	$text = str_replace('</program>', '</program>
+		$text = str_replace('</program>', '</program>
 XML;
 ?>', $text);
 	
-	echo $text;
-	
-	if ($text != '') {
+		echo $text;
+
 		$fh = fopen($localfileurl, 'w');
 		fwrite($fh, $text);
 		fclose($fh);
