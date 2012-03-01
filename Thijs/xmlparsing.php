3accbf26373f5d0231c524178145d59a3a4d8bba
Thijs van der Velden <Thijs_velden@msn.com>
2012-03-01 12:56:18 +0100

    de inserts geüpdatet laatste werkt nog niet goed

--- Thijs/xmlparsing.php
+++ Thijs/xmlparsing.php
@@ -21,14 +21,96 @@ for($n=1; $n<1100; $n++)
 					echo $profile . '<br />';
 				}
 
-					if( $xml->programClassification->admissableProgram[$x]->additionalSubject != '' )
-					{
-						$additionalSubject = $xml->programClassification->admissableProgram[$x]->additionalSubject;
-						echo $additionalSubject . '<br />';
-					}	
+				if( $xml->programClassification->admissableProgram[$x]->additionalSubject != '' )
+				{
+					$additionalSubject = $xml->programClassification->admissableProgram[$x]->additionalSubject;
+					echo $additionalSubject . '<br />';
+				}	
 			}
 		}		
 		echo '<br />';
+		
+		// insert degree, financing, credits, duration, form, id, level, cluster
+		if ($xml->programClassification->degree != '')
+		{
+				
+				if( $xml->programClassification->degree != '' )
+				{
+					$degree = $xml->programClassification->degree;
+					echo $degree . '<br />';
+				}
+
+				if( $xml->programClassification->financing != '' )
+				{
+					$financing = $xml->programClassification->financing;
+					echo $financing . '<br />';
+				}	
+				
+				if( $xml->programClassification->programCredits != '' )
+				{
+					$credits = $xml->programClassification->programCredits;
+					echo $credits . '<br />';
+				}	
+				
+				if( $xml->programClassification->programDuration != '' )
+				{
+					$duration = $xml->programClassification->programDuration;
+					echo $duration . '<br />';
+				}	
+				
+				if( $xml->programClassification->programDuration != '' )
+				{
+					$duration = $xml->programClassification->programDuration;
+					echo $duration . '<br />';
+				}	
+
+				if( $xml->programClassification->programForm != '' )
+				{
+					$programForm = $xml->programClassification->programForm;
+					echo $programForm . '<br />';
+				}	
+				
+				if( $xml->programClassification->programId != '' )
+				{
+					$programId = $xml->programClassification->programId;
+					echo $programId . '<br />';
+				}	
+				
+				if( $xml->programClassification->programLevel != '' )
+				{
+					$programLevel = $xml->programClassification->programLevel;
+					echo $programLevel . '<br />';
+				}
+				
+				if( $xml->programClassification->studyCluster != '' )
+				{
+					$studyCluster = $xml->programClassification->studyCluster;
+					echo $studyCluster . '<br />';
+				}
+				
+		}		
+		
+		echo '<br />';
+		
+		if ($xml->programCurriculum->course[0]->courseID != '')
+		{	
+			for($y=0; $y<10; $y++)
+			{
+				
+				if( $xml->programCurriculum->course[$y]->courseID != '' )
+				{
+					$courseID = $xml->programCurriculum->course[$y]->courseID;
+					echo $courseID . '<br />';
+				}
+
+				if( $xml->programCurriculum->course[$y]->courseName != '' )
+				{
+					$courseName = $xml->programCurriculum->course[$y]->courseName;
+					echo $courseName . '<br />';
+				}	
+			}
+		}		
+		echo '<br />';			
 		echo '<br />';
 	} 
 }
